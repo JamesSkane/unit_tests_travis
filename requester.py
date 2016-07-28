dafa1437dbafe7a09efa184b5e525560611c8a13
JamesSkane <jrskane@dons.usfca.edu>
2016-07-28 15:13:01 -0700

    Finished first function, still working on 2nd and 3rd, and testcases

--- requester.py
+++ requester.py
@@ -1,24 +1,122 @@
+from pandas.io.common import CParserError
+from urllib2 import HTTPError
+from pandas.core.common import PandasError
+import os
 import pandas as pd
+import numpy as np
 import csv
 import requests
 
+
 def get_url(address):
+    """
+    Called by url_to_csv, Raises a ValueError for bad URL
+     """
+
     response = requests.get(address)
     if response.status_code != 200:
-        return 'ValueError: Invalid Web Address'
-    else:
-        return response.status_code
-
-def check_data(address):
-    frame = url_to_df(address)
-    if '&' in frame.iloc[1, 1]:
-        return 'TypeError: URL cannot be made into csv'
+        raise ValueError('The url entered does not exist. Must enter valid url.')
     else:
         pass
 
+def validate_csv(csv_file):
+    """
+    Checks that the csv file being created is not html
+    """
+
+    frame = pd.read_csv(csv_file)
+    cols = frame.columns.tolist()
+    for i in range(len(cols)):
+        series = frame[cols[i]].astype(str)
+        if np.any(series.str.contains('html')):
+            os.remove(csv_file)
+            raise RuntimeWarning('URL address is not in csv format, column %i contains html.' % i)
+        else:
+            pass
+
+
+def url_to_csv(url,name='csv_example'):
+    """
+    Raises a TypeError for urls that cannot be parsed into csv format
+    """
+
+    get_url(url)
+    try:
+        frame = pd.read_csv(url, header=None)
+        name = "{}.csv".format(name)
+        frame.to_csv(name)
+        validate_csv(url)
+    except Exception:
+        os.remove(name)
+        raise TypeError('The url entered cannot be parsed into a csv.')
+    return os.path.abspath(name)
+
+
+
+def batch_url_to_csv(url_list, name_lst):
+    """
+    Raises a RuntimeWarning for bad URLs and/or urls that cannot be parsed into csv format.
+    """
+
+    title_lst = []
+    new_name_lst = []
+    for address, name in zip(url_list, name_lst):
+        name = url_to_csv(address, name)
+        new_name_lst.append(name)
+    for f in new_name_lst:
+        title = "{}.csv".format(f)
+        title_lst.append(title)
+    return title_lst
+
+
+
+def url_to_df(url):
+    """
+    Raises RuntimeWarning if size of dataframe does not match the size of csv without headers,
+    Also raises ValueError if invalid url passed as an argument.
+    """
+
+    try:
+        frame = pd.read_csv(url, header=None)
+        frame_length_pd = len(frame)
+        frame_length_csv = len(pd.read_csv(url,header=None))#url_to_csv(url)
+        if frame_length_pd == frame_length_csv:
+            return frame
+        else:
+            raise RuntimeWarning('Number of rows in dataframe do not match the number of rows in csv when there is no header row in the csv.')
+    except HTTPError:
+        raise ValueError('The url entered cannot be parsed into a csv')
 
 def add(x, y):
     return x + y
+
+
+
+
+
+
+
+#url_to_csv('https://archive.ics.uci.edu/ml/machine-learning-databazes/car', 'cars')
+#url_to_csv('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary')
+#url_to_csv('https://www.yahoo.com/')
+CSV_URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/balloons/yellow-small.data'#'https://archive.ics.uci.edu/ml/machine-learning-databases/car'
+#csv_lst = ['https://archive.ics.uci.edu/ml/machine-learning-databases/balloons/yellow-small.data',
+#             'http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv']
+#file_ex = url_to_csv(CSV_URL, name='sacramento')
+fname=url_to_csv(CSV_URL, 'balloons')
+
+print len(pd.read_csv(fname))
+print len(url_to_df(CSV_URL))
+#validate_csv('/Users/desert/unit_tests_travis/balloons.csv')
+#df = url_to_df(CSV_URL)#, name='sacramento')
+#print df.head()
+
+
+#print(df.head())
+#fs = batch_url_to_csv(csv_lst, ['balloons', 'sacramento'])
+
+
+
 
 # def url_to_csv(CSV_URL=None, name='csv_example'):
 #     with requests.Session() as s:
@@ -35,73 +133,46 @@ def add(x, y):
 #         #print(len(my_list))
 #         for row in my_list:
 #             outputWriter.writerow(row)
-
-
-
-def batch_url_to_csv(url_list, name_lst):
-    title_lst = []
-    new_name_lst = []
-    for address, name in zip(url_list, name_lst):
-        url_to_csv(address, name)
-        new_name_lst.append(name)
-    for f in new_name_lst:
-        title = "{}.csv".format(f)
-        title_lst.append(title)
-    return title_lst
-
-def url_to_df(url):
-    frame= pd.read_csv(url, header=None)
-    return frame
-
-
-def url_to_csv(url,name='csv_example'):
-    print 'hi'
-    try:
-        print 'hello'
-        frame = pd.read_csv(url, header=None)
-        print(frame.head())
-        print url
-        result = frame.to_csv(name)
-        res = csv.reader(result.splitlines(), delimiter=',')
-        return res
-    except Exception:
-        print 'bye'
-        raise ValueError
-
-
-
-
-# # import unittest
-# #
-# # def fun(x):
-# #     return x + 1
-# #
-# # class MyTest(unittest.TestCase):
-# #     def test(self):
-# #         self.assertEqual(fun(3), 4)
-# #
-# #
-# #
-# # # if __name__ == '__main__':
-# # #     unittest.main()
-# import pandas as pd
 #
-# def url_to_df(url):
-#     frame= pd.read_csv(url, header=None)
-#     return frame
 #
-# def url_to_csv(url):
-#     frame= pd.read_csv(url)
-#     #frame.to_csv()
+# Direcory structure: does unittest discovery require __init__ and sub dir?
+#df_rows = url_to_df('http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv')
+#print df_rows
 #
-# df = url_to_df('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data')
-# print df.head()
+# def get_url(address):
+#     """Called by url_to_csv, Raises a ValueError for bad URL """
+#     response = requests.get(address)
+#     if response.status_code != 200:
+#         try:
+#         # something
+#         except ValueError as e:
 #
-# import requests
+#             # clean up
+#             raise AssertionError('something went wrong')
 #
-# response = requests.get('http://google.com')
-# print response.status_code < 400
-# #assert response.status_code < 400
+#
+#         else:
+#             pass
+#
+#             # ck csv
 
-url_to_csv('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary')
 
+# def get_url2(address):
+#     """Called by url_to_csv, Raises a ValueError for bad URL """
+#     response = requests.get(address)
+#     if response.status_code != 200:
+#         raise RuntimeWarning
+#     else:
+#         pass
+#
+#
+# def url_to_csv2(url,name='csv_example'):
+#     """Raises a TypeError for urls that cannot be parsed into csv format"""
+#     get_url(url)
+#     try:
+#         frame = pd.read_csv(url, header=None)
+#         return frame
+#     except :
+#         raise RuntimeWarning
+#     except ZeroDivisionError:
+#         raise
--- tester.py
+++ tester.py
@@ -1,24 +1,42 @@
-import unittest
 import numpy as np
-from requester import add, url_to_csv, get_url
+import requests, csv, unittest
 from numpy.testing import assert_array_almost_equal
-import requests
-from urlparse import urlparse
-
-
-
+from requester import add, url_to_csv, get_url, url_to_df
 
 
 class TestNumComponentsReturned(unittest.TestCase):
 
-    def test_url(self):
+
+    def test_good_url(self):
         response = get_url('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data')
-        self.assertEqual(response, 200)
+        self.assertEqual(response, None)
 
     def test_bad_url(self):
-        with self.assertRaises(TypeError):
-            url_to_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/')
+        with self.assertRaises(ValueError):
+            url_to_csv('https://archive.ics.uci.edu/ml/machine-bearning-databases/car/')
 
+    def test_url_to_csv_fails_csv_parse(self):
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/balloons'
+        # url='http://www.yahoo.com'
+        with self.assertRaises(TypeError):#, msg="URL cannot be parsed as CSV"):
+            url_to_csv(url)
+
+
+    def test_url_to_df_DataFrame_rows(self):
+        url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data'
+        df_rows = url_to_df(url).shape[0]
+        with requests.Session() as s:
+            download = s.get(url)
+            decoded_content = download.content.decode('utf-8')
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+            my_list = list(cr)
+            csv_rows = len(my_list)
+        self.assertEqual(df_rows, csv_rows)
+
+    # def test_url_to_csv_invalid_url(self):
+    #     url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.2_week.csv'
+    #     with self.assertRaises(ValueError):
+    #         url_to_csv(url, 'tmp.csv')
 
     def test_add(self):
         res = add(3, 4)
@@ -33,10 +51,16 @@ class TestNumComponentsReturned(unittest.TestCase):
         arr2 = np.array([0.0, 0.10000089, 0.15])
         assert_array_almost_equal(arr1, arr2, decimal=6)
 
+
+
+
+
 class TestSomeOtherThing(unittest.TestCase):
 
     def test_something_else(self):
         pass
+
+
 
 
 if __name__ == '__main__':
